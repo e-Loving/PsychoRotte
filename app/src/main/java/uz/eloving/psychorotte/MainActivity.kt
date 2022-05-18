c1b7b5b1bdb88405507829e4835c6da30bfc7e3b
Islom <104727715+e-Loving@users.noreply.github.com>
2022-05-18 17:16:39 +0500

    Starting of fragments

--- app/src/main/java/uz/eloving/psychorotte/MainActivity.kt
+++ app/src/main/java/uz/eloving/psychorotte/MainActivity.kt
@@ -1,11 +1,28 @@
 package uz.eloving.psychorotte
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import uz.eloving.psychorotte.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navView.setupWithNavController(navController)
     }
 }
